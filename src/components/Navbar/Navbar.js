62d03f9bbda6c83dd0d4b65d8cbeac8b4a1fb0c3
LouqHI <h.islamiya75@gmail.com>
2022-12-01 09:20:36 +0100

    ajout du logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/apropos" >A propos</Link>
-        </nav>
+        <header>
+            <img src={logo} alt="Logo kasa " />
+            <nav>
+                <Link to="/">Accueil</Link>
+                <Link to="/apropos" >A propos</Link>
+            </nav>
+        </header>
     )
 }
