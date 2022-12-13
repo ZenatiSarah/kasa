b86345e6c65c3daacb60802de8c1ce2d503fd005
LouqHI <h.islamiya75@gmail.com>
2022-12-13 07:19:25 +0100

    ajout de la page 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import '../style/error.css'
+import logo from '../assets/404.png'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
     return (
-        <h1>Page 404 Error</h1>
+        <main className='error'>
+            <div className='img_error'>
+                <img src={logo} alt='Page introuvable' />
+                <h1>Oups! La page que vous demandez n'existe pas.</h1>
+                <Link to='/' className='link'>Retourner sur la page d’accueil </Link>
+            </div>
+
+
+
+        </main>
+
     )
 }
