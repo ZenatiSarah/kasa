4fe62c1bd4cd88faa022302b13743694d0478d3d
ZenatiSarah <62511328+ZenatiSarah@users.noreply.github.com>
2023-01-17 11:02:14 +0100

    page about terminé

--- src/components/Apropos/Collapse.js
+++ src/components/Apropos/Collapse.js
@@ -1,40 +1,29 @@
 import React from 'react'
 import arrow from '../../assets/Vector.png'
-import { detailCollapse } from "../../data/Apropos"
 import { useState } from 'react'
 
 
 
-export default function Collapse() {
-    const [isOpen, setIsOpen] = useState(false)
-    return isOpen ? (
-        <div className='collapse'> {detailCollapse.map((collapse) => (
+export default function Collapse({ titre, description }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className='collapse'>
             <section>
                 <div className='collapse_title'>
-                    <h1 className='collapse_txt'>{collapse.title}</h1>
+                    <h1 className='collapse_txt'>{titre}</h1>
                     <div className='collapse_fleche'>
-                        <img src={arrow} alt="fleche ouverture/fermeture" onClick={() => setIsOpen(false)} />
+                        <img src={arrow} alt="fleche ouverture/fermeture" onClick={() => setIsOpen(!isOpen)} />
 
                     </div>
                 </div>
-                <div className='collapse_texte'>
-                    <p>{collapse.texte}</p>
-                </div>
-            </section>
-
-        ))}
-        </div>
-    ) : (
-        <div className='collapse'> {detailCollapse.map((collapse) => (
-            <section>
-                <div className='collapse_title'>
-                    <h1 className='collapse_txt'>{collapse.title}</h1>
-                    <div className='collapse_fleche'>
-                        <img src={arrow} alt="fleche ouverture/fermeture" onClick={() => setIsOpen(true)} />
+                {
+                    isOpen && <div className='collapse_texte'>
+                        <p>{description}</p>
                     </div>
-                </div>
+                }
+
             </section>
-        ))}
         </div>
     )
 }
